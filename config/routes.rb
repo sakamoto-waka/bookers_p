9c7e01945a8936ee1f3b01888167ef68d4202aa2
EC2 Default User <ec2-user@ip-172-31-32-12.ec2.internal>
2022-02-08 02:04:26 +0000

    [add]home,index page layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'books/new' 
-  get 'books/index'
+  get 'books/index' => 'books#index'
   get 'books/show'
   get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
