d674e3d2abc994e376ae0a9f58b8e4ece68623c1
sakamoto-waka <wakanmhl@gmail.com>
2022-02-11 14:22:45 +0000

    [add]destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  get 'books/new'
-  get 'books', to: 'books#index'
-  post 'books', to: 'books#create'
-  get 'books/:id', to: 'books#show', as: 'book'
-  get 'books/:id/edit', to: 'books#edit', as:'edit_book'
-  patch 'book/:id', to: 'books#update', as: 'update_book'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
