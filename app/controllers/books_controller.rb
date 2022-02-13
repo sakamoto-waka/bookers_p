dfd2e9d473852016d064eaca4af1e61faff2304b
sakamoto-waka <wakanmhl@gmail.com>
2022-02-13 04:38:53 +0000

    [add] flash message for update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,15 +23,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice: 'Book was successfully updated.'
+    # @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end  
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path, notice: 'Book was successfully destroyed'
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
 
   private
